3d8d91e48586931315977a42cf0c166ba26f935e
horrible little slime <69secret69email69@gmail.com>
2021-06-22 00:04:19 -0400

    push katarn's ascension changes

--- src/ascend.ts
+++ src/ascend.ts
@@ -1,119 +1,212 @@
-import { visitUrl, toInt, myPrimestat, containsText, print } from "kolmafia";
+import { visitUrl, toInt, containsText, print } from "kolmafia";
 import { $item, $class, $items, $stat } from "libram";
 
+export enum lifestyle {
+    casual = 1,
+    softcore = 2,
+    normal = 2,
+    hardcore = 3,
+}
+
+export enum path {
+    unrestricted = 0,
+    boozetafarian = 1,
+    booze = 1,
+    teetotaler = 2,
+    teet = 2,
+    oxygenarian = 3,
+    oxy = 3,
+    beesHateYou = 4,
+    bhy = 4,
+    wayOfTheSurprisingFist = 6,
+    wotsf = 6,
+    trendy = 7,
+    avatarOfBoris = 8,
+    aob = 8,
+    boris = 8,
+    bugbearInvasion = 9,
+    bugbear = 9,
+    zombieSlayer = 10,
+    zombie = 10,
+    zs = 10,
+    classAct = 11,
+    classAct1 = 11,
+    classActI = 11,
+    avatarOfJarlsberg = 12,
+    aoj = 12,
+    jarlsberg = 12,
+    big = 14,
+    bigExclamationPoint = 14,
+    kolhs = 15,
+    hs = 15,
+    classAct2 = 16,
+    classActII = 16,
+    avatarOfSneakyPete = 17,
+    aosp = 17,
+    sneakyPete = 17,
+    pete = 17,
+    slowAndSteady = 18,
+    heavyRains = 19,
+    hr = 19,
+    picky = 21,
+    standard = 22,
+    actuallyEdTheUndying = 23,
+    aetu = 23,
+    ed = 23,
+    edTheUndying = 23,
+    avatarOfEdTheUndying = 23,
+    aoetu = 23,
+    oneCrazyRandomSummer = 24,
+    ocrs = 24,
+    communityService = 25,
+    cs = 25,
+    katarnCore = 25,
+    avatarOfWestOfLoathing = 26,
+    aowol = 26,
+    wol = 26,
+    theSource = 27,
+    source = 27,
+    nuclearAutumn = 28,
+    na = 28,
+    gelatinousNoob = 29,
+    gn = 29,
+    licenseToAdventure = 30,
+    lta = 30,
+    liveAscendRepeat = 31,
+    lar = 31,
+    liveDotAscendDotRepeatDot = 31,
+    pocketFamiliars = 32,
+    pokeFam = 32,
+    pokemon = 32,
+    porkemann = 32,
+    gLover = 33,
+    donaldGlover = 33,
+    disguisesDelimit = 34,
+    darkGyffte = 35,
+    dg = 35,
+    vampyre = 35,
+    vampire = 35,
+    twoCrazyRandomSummer = 36,
+    tcrs = 36,
+    kingdomOfExploathing = 37,
+    koe = 37,
+    exploathing = 37,
+    pathOfThePlumber = 38,
+    plumber = 38,
+    potp = 38,
+    avatarOfThePlumber = 38,
+    aotp = 38,
+    itsAMeMario = 38,
+    lowKeySummer = 39,
+    lks = 39,
+    lokiSummer = 39,
+    greyGoo = 40,
+    grayGoo = 40,
+    prismersDilemma = 40,
+    youRobot = 41,
+    yr = 41,
+    cogmind = 41,
+    quantumTerrarium = 42,
+    qt = 42,
+    onlyFamiiars = 42,
+}
+
 export function ascend(
-    path: string | number,
+    pathId: number,
     playerClass: Class,
-    core: string | number,
+    lifestyle: lifestyle,
     moon: string | number,
-    consumable: Item = $item`astral six-pack`,
-    pet: Item = $item`none`
+    consumable: Item | undefined = $item`astral six-pack`,
+    pet: Item | undefined = undefined
 ) {
     if (!containsText(visitUrl("charpane.php"), "Astral Spirit")) {
         print("It'd really be better if you were already through the gash. Oh well!", "blue");
         visitUrl("ascend.php?action=ascend&confirm=on&confirm2=on");
     }
     if (!containsText(visitUrl("charpane.php"), "Astral Spirit")) throw "Failed to ascend.";
+    const avatarPathIds = [8, 10, 12, 17, 23, 26, 29, 35, 38];
+    const toMoonId = (moon: string | number): number => {
+        if (typeof moon === "number") return moon;
 
-    const avatarPaths = [8, 10, 12, 17, 23, 26, 29, 35, 38];
-    const paths = new Map<string, number>([
-        ["unrestricted", 0],
-        ["boozetarian", 1],
-        ["teetotaler", 2],
-        ["oxygenarian", 3],
-        ["bees hate you", 4],
-        ["way of the surprising fist", 6],
-        ["trendy", 7],
-        ["avatar of boris", 8],
-        ["bugbear invasion", 9],
-        ["zombie slayer", 10],
-        ["class act", 11],
-        ["avatar of jarlsberg", 12],
-        ["big!", 14],
-        ["kolhs", 15],
-        ["class act ii: a class for pigs", 16],
-        ["avatar of sneaky pete", 17],
-        ["slow and steady", 18],
-        ["heavy rains", 19],
-        ["picky", 21],
-        ["standard", 22],
-        ["actually ed the undying", 23],
-        ["one crazy random summer", 24],
-        ["community service", 25],
-        ["avatar of west of loathing", 26],
-        ["the source", 27],
-        ["nuclear autumn", 28],
-        ["gelatinous noob", 29],
-        ["license to adventure", 30],
-        ["live. ascend. repeat.", 31],
-        ["pocket familiars", 32],
-        ["g-lover", 33],
-        ["disguises delimit", 34],
-        ["dark gyffte", 35],
-        ["two crazy random summer", 36],
-        ["kingdom of exploathing", 37],
-        ["path of the plumber", 38],
-        ["low key summer", 39],
-        ["grey goo", 40],
-        ["you, robot", 41],
-    ]);
-    const cores = new Map<string, number>([
-        ["casual", 1],
-        ["softcore", 2],
-        ["normal", 2],
-        ["hardcore", 3],
-    ]);
-    const moons = new Map<string, number>([
-        ["mongoose", 1],
-        ["wallaby", 2],
-        ["vole", 3],
-        ["platypus", 4],
-        ["opossum", 5],
-        ["marmot", 6],
-        ["wombat", 7],
-        ["blender", 8],
-        ["packrat", 9],
-        [
-            "knoll",
-            myPrimestat() === $stat`muscle` ? 1 : myPrimestat() === $stat`mysticality` ? 2 : 3,
-        ],
-        [
-            "canada",
-            myPrimestat() === $stat`muscle` ? 4 : myPrimestat() === $stat`mysticality` ? 5 : 6,
-        ],
-        [
-            "gnomads",
-            myPrimestat() === $stat`muscle` ? 7 : myPrimestat() === $stat`mysticality` ? 8 : 9,
-        ],
-    ]);
-    const moonid = typeof moon === "number" ? moon : moons.get(moon.toLowerCase());
-    const coreid = typeof core === "number" ? core : cores.get(core.toLowerCase());
-    const pathid = typeof path === "number" ? path : paths.get(path.toLowerCase());
+        const offset = (): number => {
+            switch (playerClass.primestat) {
+                case $stat`Muscle`:
+                    return 0;
+                case $stat`Mysticality`:
+                    return 1;
+                case $stat`Moxie`:
+                    return 2;
+                default:
+                    throw `unknown prime stat for ${playerClass}`;
+            }
+        };
+
+        switch ((moon as string).toLowerCase()) {
+            case "mongoose":
+                return 1;
+            case "wallaby":
+                return 2;
+            case "vole":
+                return 3;
+            case "platypus":
+                return 4;
+            case "opossum":
+                return 5;
+            case "marmot":
+                return 6;
+            case "wombat":
+                return 7;
+            case "blender":
+                return 8;
+            case "packrat":
+                return 9;
+            case "degrassi":
+            case "degrassi knoll":
+            case "friendly degrassi knoll":
+            case "knoll":
+                return 1 + offset();
+            case "canada":
+            case "canadia":
+            case "little canadia":
+                return 4 + offset();
+            case "gnomads":
+            case "gnomish":
+            case "gnomish gnomads camp":
+                return 7 + offset();
+            default:
+                return -1;
+        }
+    };
     const classid = toInt(playerClass);
+    if (pathId < 0) throw `Invalid path ID ${pathId}`;
+    if (toMoonId(moon) < 1) throw `Invalid moon ${moon}`;
+    if (playerClass === $class`none` || toInt(playerClass) > 6)
+        throw `Unsupported class ${playerClass}`;
+    if (avatarPathIds.includes(pathId)) throw `Unsupported path ${pathId}`;
+    if (consumable && !$items`astral six-pack, astral hot dog dinner`.includes(consumable))
+        throw `Invalid consumable ${consumable}`;
     if (
-        !pathid ||
-        !coreid ||
-        !moonid ||
-        !Array.from(cores.values()).includes(coreid) ||
-        !Array.from(moons.values()).includes(moonid) ||
-        !Array.from(paths.values()).includes(pathid) ||
-        (playerClass === $class`none` && !avatarPaths.includes(pathid)) ||
-        !$items`astral six-pack, astral hot dog dinner, none`.includes(consumable) ||
-        !$items`astral bludgeon, astral shield, astral chapeau, astral bracer, astral longbow, astral shorts, astral mace, astral ring, astral statuette, astral pistol, astral mask, astral pet sweater, astral shirt, astral belt, none`.includes(
-            pet
-        )
+        pet &&
+        !$items`astral bludgeon,
+    astral shield, astral chapeau,
+    astral bracer, astral longbow,
+    astral shorts, astral mace,
+    astral ring, astral statuette,
+    astral pistol, astral mask,
+    astral pet sweater, astral shirt,
+    astral belt`.includes(pet)
     )
-        throw "Invalid selection!";
+        throw `Invalid astral item ${pet}`;
 
     visitUrl("afterlife.php?action=pearlygates");
 
-    if (consumable !== $item`none`)
-        visitUrl(`afterlife.php?action=buydeli&whichitem=${toInt(consumable)}`);
-
-    if (pet !== $item`none`) visitUrl(`afterlife.php?action=buyarmory&whichitem=${toInt(pet)}`);
+    if (consumable) visitUrl(`afterlife.php?action=buydeli&whichitem=${toInt(consumable)}`);
+    if (pet) visitUrl(`afterlife.php?action=buyarmory&whichitem=${toInt(pet)}`);
 
     visitUrl(
-        `afterlife.php?action=ascend&confirmascend=1&whichsign=${moonid}&gender=2&whichclass=${classid}&whichpath=${pathid}&asctype=${coreid}&nopetok=1&noskillsok=1&pwd`,
+        `afterlife.php?action=ascend&confirmascend=1&whichsign=${toMoonId(
+            moon
+        )}&gender=2&whichclass=${classid}&whichpath=${pathId}&asctype=${lifestyle}&nopetok=1&noskillsok=1&pwd`,
         true
     );
 }
