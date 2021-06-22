5748d8acb7a41a1baeb081d8a44076c66cbc5a05
horrible little slime <69secret69email69@gmail.com>
2021-06-22 00:15:36 -0400

    add a gash hopper

---
+++ src/gash-hop.ts
@@ -0,0 +1,19 @@
+import { cliExecute, getCampground, getWorkshed, myAdventures, use, visitUrl } from "kolmafia";
+import { $item, have, get, $class } from "libram";
+import { ascend, lifestyle, path } from "./ascend";
+
+cliExecute("breakfast");
+if (myAdventures() > 0) throw "You sure about that one, cowboy?";
+
+const car = $item`asdon-martin key fob`;
+const garden = $item`peppermint pip packet`;
+
+if (getWorkshed() !== car && have(car) && !get("_workshedItemUsed")) use(car);
+if (getWorkshed() !== car) throw "Unable to get the car!";
+
+if (!Object.getOwnPropertyNames(getCampground()).includes(garden.name) && have(garden)) use(garden);
+if (!Object.getOwnPropertyNames(getCampground()).includes(garden.name))
+    throw "Unable to plant peppermint!";
+
+visitUrl("ascend.php?action=ascend&confirm=on&confirm2=on");
+ascend(path.katarnCore, $class`pastamancer`, lifestyle.hardcore, "knoll", $item`astral six-pack`);
