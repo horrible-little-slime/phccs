1117634860650d8c865bd65a50e86f41406165e1
horrible little slime <69secret69email69@gmail.com>
2021-08-06 01:35:31 -0400

    return all to normal, probably

--- src/gash-hop.ts
+++ src/gash-hop.ts
@@ -1,13 +1,41 @@
-import { $class, $item, ascend, Lifestyle, Path, Paths, prepareAscension } from "libram";
+import {
+    buy,
+    cliExecute,
+    eudora,
+    getCampground,
+    getChateau,
+    getWorkshed,
+    myAdventures,
+    use,
+    visitUrl,
+} from "kolmafia";
+import { $item, have, get, $class } from "libram";
+import { ascend, Lifestyle } from "./ascend";
+import { Paths } from "./Path";
 
-prepareAscension({
-    workshed: $item`Asdon Martin keyfob`,
-    garden: $item`Peppermint pip packet`,
-    eudora: $item`Our Daily Candles™ order form`,
-}, {
-    desk: $item`continental juice bar`,
-    ceiling: $item`ceiling fan`,
-    nightstand: $item`foreign language tapes`,
-});
+cliExecute("breakfast");
+if (myAdventures() > 0) throw "You sure about that one, cowboy?";
 
-ascend(Paths.CommunityService, $class`pastamancer`, Lifestyle.hardcore, "knoll", $item`astral six-pack`);
+const car = $item`Asdon Martin keyfob`;
+const garden = $item`Peppermint Pip Packet`;
+
+if (getWorkshed() !== car && have(car) && !get("_workshedItemUsed")) use(car);
+if (getWorkshed() !== car) throw "Unable to get the car!";
+
+if (!Object.getOwnPropertyNames(getCampground()).includes(garden.name) && have(garden)) use(garden);
+if (!Object.getOwnPropertyNames(getCampground()).includes(garden.name))
+    throw "Unable to plant peppermint!";
+
+if (eudora() !== "Our Daily Candles") eudora("Candles");
+
+if (!getChateau()["foreign language tapes"]) buy($item`foreign language tapes`);
+if (!getChateau()["ceiling fan"]) buy($item`ceiling fan`);
+
+visitUrl("ascend.php?action=ascend&confirm=on&confirm2=on");
+ascend(
+    Paths.CommunityService,
+    $class`pastamancer`,
+    Lifestyle.hardcore,
+    "knoll",
+    $item`astral six-pack`
+);
