393a031024177173819bd11f7a20df499ec445b9
horrible little slime <69secret69email69@gmail.com>
2021-08-31 01:48:00 -0400

    try katarn's webpack stuff

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,9 @@ module.exports = {
         phccs: "./src/index.ts",
         phccs_gash: "./src/gash-hop.ts",
     },
-    mode: "development",
+    // Turns on tree-shaking and minification in the default Terser minifier
+    // https://webpack.js.org/plugins/terser-webpack-plugin/
+    mode: "production",
     devtool: false,
     output: {
         // Change the final string here to the name you want your script to use in mafia.
@@ -28,6 +30,14 @@ module.exports = {
                 loader: "babel-loader",
             },
         ],
+    },
+    optimization: {
+        // Disable compression because it makes debugging more difficult for KolMafia
+        minimize: false,
+    },
+    performance: {
+        // Disable the warning about assets exceeding the recommended size because this isn't a website script
+        hints: false,
     },
     plugins: [],
     externals: {
