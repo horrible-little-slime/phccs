1a6f37ee8c48d26f6ac3864bbc8c1dd16479e55f
O.A.Ph <>
2021-09-07 04:57:21 +0000

    Build Mafia files for commit 2798d2b99dbdd7fcf5e89487cdc8dae8437aadf2

--- scripts/phccs/phccs.js
+++ scripts/phccs/phccs.js
@@ -19552,10 +19552,8 @@ PropertyManager.set({
   customCombatScript: "twiddle",
   battleAction: "custom combat script",
   dontStopForCounters: true,
-  hpAutoRecovery: undefined,
-  hpAutoRecoveryTarget: -0.5,
-  mpAutoRecovery: undefined,
-  mpAutoRecoveryTarget: -0.5
+  hpAutoRecovery: -0.05,
+  mpAutoRecovery: -0.05
 });
 var startTime = (0,external_kolmafia_.gametimeToInt)();
 
