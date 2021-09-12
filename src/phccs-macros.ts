80053db71df5f063d9affd3bb4e105dfb2a8ea37
horrible little slime <69secret69email69@gmail.com>
2021-09-12 15:42:19 -0400

    update candyblast more elegantly

--- src/phccs-macros.ts
+++ src/phccs-macros.ts
@@ -1,13 +1,13 @@
-import { Macro } from "libram";
+import { $skill, have, Macro } from "libram";
 
 export const delevel = Macro.skill("curse of weaksauce")
     .skill("micrometeor")
     .tryItem("time-spinner")
     .skill("summon love gnats");
 
-export const candyblast = Macro.while_(
+export const candyblast = Macro.externalIf(have($skill`Candyblast`), Macro.while_(
     '!match "Hey, some of it is even intact afterwards!"',
-    Macro.skill("candyblast")
+    Macro.skill("candyblast"))
 );
 
 export const easyFight = Macro.skill("extract").skill("sing along");
