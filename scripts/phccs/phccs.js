a0512399bbec47033ffeda7a4fc2ca7f641d71c4
O.A.Ph <>
2021-09-26 06:19:44 +0000

    Build Mafia files for commit cd1d0795f11ebd2ea893f31c7417448d01bca017

--- scripts/phccs/phccs.js
+++ scripts/phccs/phccs.js
@@ -18534,7 +18534,7 @@ function familiarTest() {
   return 60 - Math.floor(((0,external_kolmafia_.familiarWeight)((0,external_kolmafia_.myFamiliar)()) + (0,external_kolmafia_.weightAdjustment)()) / 5);
 }
 ;// CONCATENATED MODULE: ./src/hotres.ts
-var hotres_templateObject, hotres_templateObject2, hotres_templateObject3, hotres_templateObject4, hotres_templateObject5, hotres_templateObject6, hotres_templateObject7, hotres_templateObject8, hotres_templateObject9, hotres_templateObject10, hotres_templateObject11, hotres_templateObject12, hotres_templateObject13, hotres_templateObject14, hotres_templateObject15, hotres_templateObject16, hotres_templateObject17, hotres_templateObject18, hotres_templateObject19, hotres_templateObject20, hotres_templateObject21, hotres_templateObject22, hotres_templateObject23, hotres_templateObject24, hotres_templateObject25, hotres_templateObject26, hotres_templateObject27, hotres_templateObject28, hotres_templateObject29, hotres_templateObject30, hotres_templateObject31, hotres_templateObject32, hotres_templateObject33, hotres_templateObject34;
+var hotres_templateObject, hotres_templateObject2, hotres_templateObject3, hotres_templateObject4, hotres_templateObject5, hotres_templateObject6, hotres_templateObject7, hotres_templateObject8, hotres_templateObject9, hotres_templateObject10, hotres_templateObject11, hotres_templateObject12, hotres_templateObject13, hotres_templateObject14, hotres_templateObject15, hotres_templateObject16, hotres_templateObject17, hotres_templateObject18, hotres_templateObject19, hotres_templateObject20, hotres_templateObject21, hotres_templateObject22, hotres_templateObject23, hotres_templateObject24, hotres_templateObject25, hotres_templateObject26, hotres_templateObject27, hotres_templateObject28, hotres_templateObject29, hotres_templateObject30, hotres_templateObject31, hotres_templateObject32, hotres_templateObject33, hotres_templateObject34, hotres_templateObject35, hotres_templateObject36, hotres_templateObject37, hotres_templateObject38, hotres_templateObject39, hotres_templateObject40, hotres_templateObject41, hotres_templateObject42, hotres_templateObject43, hotres_templateObject44, hotres_templateObject45, hotres_templateObject46, hotres_templateObject47, hotres_templateObject48, hotres_templateObject49, hotres_templateObject50, hotres_templateObject51;
 
 function hotres_taggedTemplateLiteral(strings, raw) { if (!raw) { raw = strings.slice(0); } return Object.freeze(Object.defineProperties(strings, { raw: { value: Object.freeze(raw) } })); }
 
@@ -18582,32 +18582,33 @@ function deepDarkVisions() {
     (0,external_kolmafia_.use)(1, (0,dist.$item)(hotres_templateObject21 || (hotres_templateObject21 = hotres_taggedTemplateLiteral(["box of Familiar Jacks"]))));
   }
 
-  (0,external_kolmafia_.maximize)("spooky res", false);
+  (0,external_kolmafia_.cliExecute)("retrocape vampire hold");
+  new Outfit(new Map([[(0,dist.$slot)(hotres_templateObject22 || (hotres_templateObject22 = hotres_taggedTemplateLiteral(["hat"]))), (0,dist.$item)(hotres_templateObject23 || (hotres_templateObject23 = hotres_taggedTemplateLiteral(["Iunion Crown"])))], [(0,dist.$slot)(hotres_templateObject24 || (hotres_templateObject24 = hotres_taggedTemplateLiteral(["shirt"]))), (0,dist.$item)(hotres_templateObject25 || (hotres_templateObject25 = hotres_taggedTemplateLiteral(["denim jacket"])))], [(0,dist.$slot)(hotres_templateObject26 || (hotres_templateObject26 = hotres_taggedTemplateLiteral(["back"]))), (0,dist.$item)(hotres_templateObject27 || (hotres_templateObject27 = hotres_taggedTemplateLiteral(["unwrapped knock-off retro superhero cape"])))], [(0,dist.$slot)(hotres_templateObject28 || (hotres_templateObject28 = hotres_taggedTemplateLiteral(["weapon"]))), (0,dist.$item)(hotres_templateObject29 || (hotres_templateObject29 = hotres_taggedTemplateLiteral(["Fourth of May Cosplay Saber"])))], [(0,dist.$slot)(hotres_templateObject30 || (hotres_templateObject30 = hotres_taggedTemplateLiteral(["off-hand"]))), (0,dist.$item)(hotres_templateObject31 || (hotres_templateObject31 = hotres_taggedTemplateLiteral(["familiar scrapbook"])))], [(0,dist.$slot)(hotres_templateObject32 || (hotres_templateObject32 = hotres_taggedTemplateLiteral(["pants"]))), (0,dist.$item)(hotres_templateObject33 || (hotres_templateObject33 = hotres_taggedTemplateLiteral(["Cargo Cultist Shorts"])))], [(0,dist.$slot)(hotres_templateObject34 || (hotres_templateObject34 = hotres_taggedTemplateLiteral(["familiar"]))), (0,dist.$item)(hotres_templateObject35 || (hotres_templateObject35 = hotres_taggedTemplateLiteral(["cracker"])))], [(0,dist.$slot)(hotres_templateObject36 || (hotres_templateObject36 = hotres_taggedTemplateLiteral(["acc1"]))), (0,dist.$item)(hotres_templateObject37 || (hotres_templateObject37 = hotres_taggedTemplateLiteral(["your cowboy boots"])))]]), (0,dist.$familiar)(hotres_templateObject38 || (hotres_templateObject38 = hotres_taggedTemplateLiteral(["Exotic Parrot"])))).dress();
 
-  while ((0,dist.have)((0,dist.$skill)(hotres_templateObject22 || (hotres_templateObject22 = hotres_taggedTemplateLiteral(["Deep Dark Visions"])))) && (0,external_kolmafia_.haveEffect)((0,dist.$effect)(hotres_templateObject23 || (hotres_templateObject23 = hotres_taggedTemplateLiteral(["Visions of the Deep Dark Deeps"])))) < 30 && (0,dist.have)((0,dist.$item)(hotres_templateObject24 || (hotres_templateObject24 = hotres_taggedTemplateLiteral(["magical sausage casing"]))))) {
-    if ((0,external_kolmafia_.myMp)() < 20 && (0,dist.have)((0,dist.$item)(hotres_templateObject25 || (hotres_templateObject25 = hotres_taggedTemplateLiteral(["magical sausage casing"]))))) {
-      (0,external_kolmafia_.create)(1, (0,dist.$item)(hotres_templateObject26 || (hotres_templateObject26 = hotres_taggedTemplateLiteral(["magical sausage"]))));
-      (0,external_kolmafia_.eat)(1, (0,dist.$item)(hotres_templateObject27 || (hotres_templateObject27 = hotres_taggedTemplateLiteral(["magical sausage"]))));
+  while ((0,dist.have)((0,dist.$skill)(hotres_templateObject39 || (hotres_templateObject39 = hotres_taggedTemplateLiteral(["Deep Dark Visions"])))) && (0,external_kolmafia_.haveEffect)((0,dist.$effect)(hotres_templateObject40 || (hotres_templateObject40 = hotres_taggedTemplateLiteral(["Visions of the Deep Dark Deeps"])))) < 30 && (0,dist.have)((0,dist.$item)(hotres_templateObject41 || (hotres_templateObject41 = hotres_taggedTemplateLiteral(["magical sausage casing"]))))) {
+    if ((0,external_kolmafia_.myMp)() < 20 && (0,dist.have)((0,dist.$item)(hotres_templateObject42 || (hotres_templateObject42 = hotres_taggedTemplateLiteral(["magical sausage casing"]))))) {
+      (0,external_kolmafia_.create)(1, (0,dist.$item)(hotres_templateObject43 || (hotres_templateObject43 = hotres_taggedTemplateLiteral(["magical sausage"]))));
+      (0,external_kolmafia_.eat)(1, (0,dist.$item)(hotres_templateObject44 || (hotres_templateObject44 = hotres_taggedTemplateLiteral(["magical sausage"]))));
     }
 
     while ((0,external_kolmafia_.myHp)() < (0,external_kolmafia_.myMaxhp)()) {
-      (0,external_kolmafia_.useSkill)(1, (0,dist.$skill)(hotres_templateObject28 || (hotres_templateObject28 = hotres_taggedTemplateLiteral(["Cannelloni Cocoon"]))));
+      (0,external_kolmafia_.useSkill)(1, (0,dist.$skill)(hotres_templateObject45 || (hotres_templateObject45 = hotres_taggedTemplateLiteral(["Cannelloni Cocoon"]))));
     }
 
-    if ((0,external_kolmafia_.myMp)() < 100 && (0,dist.have)((0,dist.$item)(hotres_templateObject29 || (hotres_templateObject29 = hotres_taggedTemplateLiteral(["magical sausage casing"]))))) {
-      (0,external_kolmafia_.create)(1, (0,dist.$item)(hotres_templateObject30 || (hotres_templateObject30 = hotres_taggedTemplateLiteral(["magical sausage"]))));
-      (0,external_kolmafia_.eat)(1, (0,dist.$item)(hotres_templateObject31 || (hotres_templateObject31 = hotres_taggedTemplateLiteral(["magical sausage"]))));
+    if ((0,external_kolmafia_.myMp)() < 100 && (0,dist.have)((0,dist.$item)(hotres_templateObject46 || (hotres_templateObject46 = hotres_taggedTemplateLiteral(["magical sausage casing"]))))) {
+      (0,external_kolmafia_.create)(1, (0,dist.$item)(hotres_templateObject47 || (hotres_templateObject47 = hotres_taggedTemplateLiteral(["magical sausage"]))));
+      (0,external_kolmafia_.eat)(1, (0,dist.$item)(hotres_templateObject48 || (hotres_templateObject48 = hotres_taggedTemplateLiteral(["magical sausage"]))));
     }
 
     if (Math.round((0,external_kolmafia_.numericModifier)("spooky resistance")) < 10) {
-      ensureEffect((0,dist.$effect)(hotres_templateObject32 || (hotres_templateObject32 = hotres_taggedTemplateLiteral(["Does It Have a Skull In There??"]))));
+      ensureEffect((0,dist.$effect)(hotres_templateObject49 || (hotres_templateObject49 = hotres_taggedTemplateLiteral(["Does It Have a Skull In There??"]))));
 
       if (Math.round((0,external_kolmafia_.numericModifier)("spooky resistance")) < 10) {
         throw "Not enough spooky res for Deep Dark Visions.";
       }
     }
 
-    (0,external_kolmafia_.useSkill)(1, (0,dist.$skill)(hotres_templateObject33 || (hotres_templateObject33 = hotres_taggedTemplateLiteral(["Deep Dark Visions"]))));
+    (0,external_kolmafia_.useSkill)(1, (0,dist.$skill)(hotres_templateObject50 || (hotres_templateObject50 = hotres_taggedTemplateLiteral(["Deep Dark Visions"]))));
   }
 }
 
@@ -18619,7 +18620,7 @@ function hotres_testPrep() {
     }
 
     (0,external_kolmafia_.cliExecute)("asdonmartin drive safely");
-  }, () => ensureEffect((0,dist.$effect)(hotres_templateObject34 || (hotres_templateObject34 = hotres_taggedTemplateLiteral(["Amazing"]))))];
+  }, () => ensureEffect((0,dist.$effect)(hotres_templateObject51 || (hotres_templateObject51 = hotres_taggedTemplateLiteral(["Amazing"]))))];
 
   for (var _i = 0, _improvements = improvements; _i < _improvements.length; _i++) {
     var improvement = _improvements[_i];
@@ -18820,8 +18821,7 @@ function buffMyst() {
 }
 
 function level_castBuffs() {
-  (0,external_kolmafia_.useFamiliar)((0,dist.$familiar)(level_templateObject25 || (level_templateObject25 = level_taggedTemplateLiteral(["Left-Hand Man"]))));
-  (0,external_kolmafia_.maximize)("mp", false);
+  uniform((0,dist.$item)(level_templateObject25 || (level_templateObject25 = level_taggedTemplateLiteral(["Abracandalabra"]))));
 
   if ((0,dist.get)("_sausagesEaten") === 0) {
     (0,external_kolmafia_.cliExecute)("eat 1 magic sausage");
