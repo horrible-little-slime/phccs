e17ed5fa9db7c379491f2b8aa75f2ad430930bc1
horrible little slime <69secret69email69@gmail.com>
2022-01-13 18:50:21 -0500

    allow softcore ascending

--- src/gash-hop.ts
+++ src/gash-hop.ts
@@ -23,32 +23,35 @@ const safariTargets = [
     "Asmodais",
     "The Dictator",
 ];
+export function main(args: string): void {
+    while ($skill`Experience Safari`.timescast < get("skillLevel180") && safariTargets.length) {
+        useSkill($skill`Experience Safari`, 1, safariTargets[0]);
+        safariTargets.shift();
+    }
 
-while ($skill`Experience Safari`.timescast < get("skillLevel180") && safariTargets.length) {
-    useSkill($skill`Experience Safari`, 1, safariTargets[0]);
-    safariTargets.shift();
+    prepareAscension({
+        workshed: "Asdon Martin keyfob",
+        garden: "Peppermint Pip Packet",
+        eudora: "Our Daily Candles™ order form",
+        chateau: {
+            desk: "continental juice bar",
+            nightstand: "foreign language tapes",
+            ceiling: "ceiling fan",
+        },
+    });
+
+    const pet = have($familiar`Baby Bugged Bugbear`)
+        ? $item`astral statuette`
+        : $item`astral pet sweater`;
+
+    const lifestyle = args.includes("softcore") ? Lifestyle.softcore : Lifestyle.hardcore;
+
+    ascend(
+        Paths.CommunityService,
+        $class`Pastamancer`,
+        lifestyle,
+        "knoll",
+        $item`astral six-pack`,
+        pet
+    );
 }
-
-prepareAscension({
-    workshed: "Asdon Martin keyfob",
-    garden: "Peppermint Pip Packet",
-    eudora: "Our Daily Candles™ order form",
-    chateau: {
-        desk: "continental juice bar",
-        nightstand: "foreign language tapes",
-        ceiling: "ceiling fan",
-    },
-});
-
-const pet = have($familiar`Baby Bugged Bugbear`)
-    ? $item`astral statuette`
-    : $item`astral pet sweater`;
-
-ascend(
-    Paths.CommunityService,
-    $class`Pastamancer`,
-    Lifestyle.hardcore,
-    "knoll",
-    $item`astral six-pack`,
-    pet
-);
