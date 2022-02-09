9bac1b56e93fd82fdbd58af1c5ec9d46db0917ed
horrible little slime <69secret69email69@gmail.com>
2022-02-09 11:22:38 -0500

    create assertions to avoid compounding errors

--- src/index.ts
+++ src/index.ts
@@ -29,6 +29,10 @@ import { $effect, $item, $skill, CommunityService, get, have } from "libram";
 
 const HIGHLIGHT = isDarkMode() ? "yellow" : "blue";
 
+const assert = (action: boolean, warning: string) => {
+    if (!action) throw new Error(warning);
+};
+
 //preamble
 if (myPathId() !== 25) abort();
 visitUrl("council.php");
@@ -44,12 +48,12 @@ PropertyManager.set({
 });
 const startTime = gametimeToInt();
 try {
-    CommunityService.CoilWire.run(coilWire);
+    assert(CommunityService.CoilWire.run(coilWire, false, 60), "Failed to coil wire!");
     if (myLevel() < 13) levelUp();
-    CommunityService.Moxie.run(moxTest);
-    CommunityService.HP.run(HPTest);
-    CommunityService.Muscle.run(muscleTest);
-    CommunityService.Mysticality.run(mystTest);
+    assert(CommunityService.Moxie.run(moxTest, false, 1), "Failed to cap moxie test!");
+    assert(CommunityService.HP.run(HPTest, false, 1), "Failed to cap HP test!");
+    assert(CommunityService.Muscle.run(muscleTest, false, 1), "Failed to cap Muscle test!");
+    assert(CommunityService.Mysticality.run(mystTest, false, 1), "Failed to cap Mysticality test!");
     if (availableAmount($item`astral six-pack`) !== 0) use(1, $item`astral six-pack`);
     if (have($effect`The Magical Mojomuscular Melody`))
         cliExecute("shrug The Magical Mojomuscular Melody");
@@ -57,12 +61,21 @@ try {
     while (myInebriety() < 5) {
         drink(1, $item`astral pilsner`);
     }
-    CommunityService.BoozeDrop.run(itemTest);
-    CommunityService.HotRes.run(hotTest);
-    CommunityService.Noncombat.run(noncombatTest);
-    CommunityService.FamiliarWeight.run(familiarTest);
-    CommunityService.WeaponDamage.run(weaponTest);
-    CommunityService.SpellDamage.run(spellTest);
+    assert(CommunityService.BoozeDrop.run(itemTest, false, 1), "Failed to cap Item test!");
+    assert(CommunityService.HotRes.run(hotTest, false, 1), "Failed to cap Hot Res test!");
+    assert(CommunityService.Noncombat.run(noncombatTest, false, 1), "Failed to cap NC test!");
+    assert(
+        CommunityService.FamiliarWeight.run(familiarTest, false, 30),
+        "Failed to perform Familiar test!"
+    );
+    assert(
+        CommunityService.WeaponDamage.run(weaponTest, false, 1),
+        "Failed to cap Weapon Damage test!"
+    );
+    assert(
+        CommunityService.SpellDamage.run(spellTest, false, 30),
+        "Failed to perform Spell Damage test!"
+    );
 } finally {
     for (const [name, { predictedTurns, turnCost }] of Object.entries(CommunityService.log)) {
         const truePrediction = name === "Make Sausage" ? predictedTurns + 1 : predictedTurns;
