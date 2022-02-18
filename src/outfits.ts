b54a3a0c6450b5ed4f1e1310a9e20b32c0867e20
horrible little slime <69secret69email69@gmail.com>
2022-02-18 00:14:35 -0500

    astral chapeau support

--- src/outfits.ts
+++ src/outfits.ts
@@ -160,7 +160,7 @@ export function withOutfit<T>(outfit: Outfit, callback: () => T): T {
 
 export default function uniform(...changes: (Item | [Item, Slot])[]): void {
     const defaultUniform = {
-        hat: $item`Iunion Crown`,
+        hat: $items`astral chapeau, Iunion Crown`,
         shirt: $item`fresh coat of paint`,
         pants: $items`pantogram pants, old sweatpants`,
         weapon: $item`Fourth of May Cosplay Saber`,
@@ -378,7 +378,7 @@ export function spellOutfit(): void {
 
     Outfit.doYourBest(
         {
-            hat: $items`sugar chapeau, Hollandaise helmet`,
+            hat: $items`sugar chapeau, astral chapeau, Hollandaise helmet`,
             weapon: !inHardcore() ? chefstaves : $item`weeping willow wand`,
             offhand: [
                 $item`Abracandalabra`,
