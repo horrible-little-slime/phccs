81cfd6877699127e5b13d4fda2bfbe02c4f94f43
horrible little slime <69secret69email69@gmail.com>
2022-03-17 14:42:11 -0400

    shuffle safari targets

--- src/gash-hop.ts
+++ src/gash-hop.ts
@@ -24,6 +24,13 @@ const safariTargets = [
     "The Dictator",
 ];
 export function main(args = ""): void {
+    for (let i = safariTargets.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        const temp = safariTargets[i];
+        safariTargets[i] = safariTargets[j];
+        safariTargets[j] = temp;
+    }
+
     while ($skill`Experience Safari`.timescast < get("skillLevel180") && safariTargets.length) {
         useSkill($skill`Experience Safari`, 1, safariTargets[0]);
         safariTargets.shift();
