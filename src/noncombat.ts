701907f32e93af87f5f7f19ac736198d07be3f8d
horrible little slime <69secret69email69@gmail.com>
2022-04-05 13:02:17 -0400

    unconsider the lobster

--- src/noncombat.ts
+++ src/noncombat.ts
@@ -4,17 +4,11 @@ import {
     cliExecute,
     equip,
     getFuel,
-    runChoice,
-    runCombat,
     use,
-    useFamiliar,
-    visitUrl,
 } from "kolmafia";
-import Macro from "./combat";
 import {
     $coinmaster,
     $effect,
-    $familiar,
     $item,
     $slot,
     AsdonMartin,
@@ -23,8 +17,8 @@ import {
     have,
 } from "libram";
 import { universalWeightBuffs } from "./familiarweight";
-import { burnLibrams, ensureEffect, fuelUp, heal, horse, setChoice } from "./lib";
-import uniform, { noncombatOutfit } from "./outfits";
+import { burnLibrams, ensureEffect, fuelUp, horse } from "./lib";
+import { noncombatOutfit } from "./outfits";
 
 const predictor = () => CommunityService.Noncombat.prediction;
 
@@ -48,26 +42,6 @@ function castBuffs() {
     horse("dark");
 }
 
-function godLobster() {
-    if (
-        !have($effect`Silence of the God Lobster`) &&
-        get("_godLobsterFights") < 3 &&
-        have($item`God Lobster's Ring`)
-    ) {
-        useFamiliar($familiar`God Lobster`);
-        equip($slot`familiar`, $item`God Lobster's Ring`);
-        uniform();
-        Macro.defaultKill().setAutoAttack();
-        heal();
-        use(3, $item`psychokinetic energy blob`);
-        setChoice(1310, 2);
-        visitUrl("main.php?fightgodlobster=1");
-        runCombat(Macro.defaultKill().toString());
-        visitUrl("choice.php");
-        runChoice(-1);
-    }
-}
-
 function testPrep() {
     noncombatOutfit();
     if (predictor() > 1) {
@@ -81,7 +55,6 @@ function testPrep() {
 
 export default function noncombatTest(): void {
     castBuffs();
-    godLobster();
     testPrep();
     if (predictor() > 1) throw "Failed to cap noncombat";
     burnLibrams();
