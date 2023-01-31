d46210de8662d1e7f99fa7823164d2c48e0bcd5a
horrible little slime <69secret69email69@gmail.com>
2023-01-31 11:18:25 -0500

    Expand Safari List

--- src/gash/lib.ts
+++ src/gash/lib.ts
@@ -1,6 +1,7 @@
-import { useSkill } from "kolmafia";
+import { myName, useSkill } from "kolmafia";
 import { $skill, get } from "libram";
 
+const lowercaseName = myName().toLowerCase();
 const safariTargets = [
     "Kenny Kamakazi",
     "Busta_Rhymes",
@@ -13,7 +14,16 @@ const safariTargets = [
     "The Dictator",
     "Malibu Stacey",
     "DanceCommander6",
-];
+    "Captain Scotch",
+    "threebullethamburgler",
+    "BurningBman",
+    "Katarn",
+    "Baden",
+    "Purple Shrimp",
+    "Butts McGruff",
+    "SSBBHax",
+].filter((n) => n.toLowerCase() !== lowercaseName);
+
 export function burnSafaris(): void {
     for (let i = safariTargets.length - 1; i > 0; i--) {
         const j = Math.floor(Math.random() * (i + 1));
