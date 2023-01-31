adcf153b9387458d05a3105653871e1061410725
horrible little slime <69secret69email69@gmail.com>
2023-01-31 09:13:59 -0500

    Can't calculate the universe that much

--- src/globaltasks.ts
+++ src/globaltasks.ts
@@ -15,7 +15,7 @@ const GLOBAL_TASKS: CSTask[] = [
     {
         name: "Numberology",
         ready: () => Object.values(reverseNumberology()).includes(69),
-        completed: () => get("_universeCalculated") >= get("skillLevel144"),
+        completed: () => get("_universeCalculated") >= Math.min(get("skillLevel144"), 3),
         do: () => cliExecute("numberology 69"),
     },
     {
